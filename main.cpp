f6e065dfd753b552385fa2b4838613a8920a75b1
daniel8060 <57083361+daniel8060@users.noreply.github.com>
2019-11-23 22:36:00 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,12 +16,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc,argv);
 
-        GameManager game ;
+        GameManager game ; //game creates a Board and a Bank and all of the players!
 
         WelcomeWindow w;
         w.show();
 
-    game.players[0]->move(9);
+    game.players[0]-> move(9);
     game.players[0] -> land();
 
 
